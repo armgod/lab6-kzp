49ced1efe5cd77db86fd56ba6968ca89b74dae73
vladhorbatiuk <vlad.gorbatyuk.1221@gmail.com>
2023-12-21 17:53:13 +0200

    bgfgb

---
+++ src/KI307Babiy/Cube.java
@@ -0,0 +1,16 @@
+package KI307Babiy;
+
+public class Cube extends Shape {
+    /**
+     *	Constructor
+     *	@param volume
+     *	@param width
+     *	@param height
+     *	@param name
+     */
+    public Cube(int volume, int width, int height, String name)
+    {
+        super(volume, width, height, name);
+    }
+}
+
---
+++ src/KI307Babiy/Sphere.java
@@ -0,0 +1,16 @@
+package KI307Babiy;
+
+public class Sphere extends Shape {
+    /**
+     *	Constructor
+     *	@param volume
+     *	@param width
+     *	@param height
+     *	@param name
+     */
+    public Sphere(int volume, int width, int height, String name)
+    {
+        super(volume, width, height, name);
+    }
+}
+
